321639a632809eaac833eecba95f1c7223f746d2
Javier Diaz <coderdiaz@gmail.com>
2016-11-17 14:28:27 -0600

    Added home interface with HTTP requests

---
+++ src/Note.ts
@@ -0,0 +1,8 @@
+export class Note {
+    id: number;
+    title: string;
+    description: string;
+    status: number;
+    created_at: string;
+    updated_at: string;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NoteProvider } from '../../providers/note-provider';
+import { Note } from '../../Note';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [ NoteProvider ]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  private notes: Note[] = [];
+
+  constructor(public navCtrl: NavController,
+    private noteProvider: NoteProvider) {}
+
+  ngOnInit(): void {
+    this.noteProvider.getNotes()
+      .then(response => {
+        this.notes = response;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('Ah ocurrido un problema');
+      });
   }
 
 }
